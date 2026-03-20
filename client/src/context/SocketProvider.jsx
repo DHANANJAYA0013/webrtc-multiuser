acab77c84163cdf183dd88f2e3d7bb84dc1dce5e
Dhananjaya <dhananjayanchan32@gmail.com>
2026-03-20 14:01:53 +0530

    render url added

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -1,3 +1,24 @@
+// import React, { createContext, useMemo, useContext } from "react";
+// import { io } from "socket.io-client";
+
+// const SocketContext = createContext(null);
+
+// export const useSocket = () => {
+//   const socket = useContext(SocketContext);
+//   return socket;
+// };
+
+// export const SocketProvider = (props) => {
+//   const socket = useMemo(() => io("localhost:8000"), []);
+
+//   return (
+//     <SocketContext.Provider value={socket}>
+//       {props.children}
+//     </SocketContext.Provider>
+//   );
+// };
+
+
 import React, { createContext, useMemo, useContext } from "react";
 import { io } from "socket.io-client";
 
@@ -9,7 +30,13 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(
+    () =>
+      io("https://webrtc-multiuser-w886.onrender.com", {
+        transports: ["websocket"],
+      }),
+    []
+  );
 
   return (
     <SocketContext.Provider value={socket}>
